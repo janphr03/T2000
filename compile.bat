8ded78c3d80fd61cbd0f01154091f4063f4e4cf1
Jan Herrmann <janpppherrmann@gmail.com>
2025-08-11 13:13:41 +0200

    init Commit

---
+++ compile.bat
@@ -0,0 +1,41 @@
+@echo off
+REM ======= Einstellungen =======
+REM LaTeX-Dateiname ohne Endung:
+set "BASENAME=T1000"
+set "DIR=C:\Users\herrmannja\Downloads\LaTexNeu\"
+set "FILE=%DIR%\%BASENAME%.tex"
+
+REM ======= Prüfung =======
+IF NOT EXIST "%FILE%" (
+    echo Datei "%FILE%" nicht gefunden.
+    exit /b 1
+)
+
+REM ======= Kompilierungsschritte =======
+echo -----------------------------------------
+echo [1/5] LaTeX: Erster Durchlauf ...
+pdflatex -interaction=nonstopmode "%FILE%"
+
+echo [2/5] makeglossaries ...
+makeglossaries "%BASENAME%"
+
+echo [3/5] biber ...
+biber "%BASENAME%"
+
+echo [4/5] LaTeX: Zweiter Durchlauf ...
+pdflatex -interaction=nonstopmode "%FILE%"
+
+echo [5/5] LaTeX: Dritter Durchlauf für finale Verzeichnisse ...
+pdflatex -interaction=nonstopmode "%FILE%"
+
+REM ======= Fertig =======
+IF ERRORLEVEL 1 (
+    echo *** Fehler beim Kompilieren. ***
+    exit /b 1
+) ELSE (
+    echo *** Kompilierung erfolgreich abgeschlossen. ***
+)
+
+pause
+
+
